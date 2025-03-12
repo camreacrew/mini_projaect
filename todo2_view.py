fddbc7ab1b942d95a9190eb314ab8ad2dec1e73c
cameracrew <kimhyojae2053@gmail.com>
2025-03-12 13:54:27 +0900

    추가

--- todo2_view.py
+++ todo2_view.py
@@ -1,10 +1,10 @@
-from flask import Flask, render_template
+from flask import Flask, Blueprint, render_template
 
-todo2 = Flask(__name__)
+todo2 = Blueprint('todo2', __name__, url_prefix='/todo2')
 
 todos = [{'tno': 1, 'title':'잠자기', 'date':'2025-03-12'},
          {'tno': 1, 'title':'미용실', 'date':'2025-03-12'}]
-@todo2.route("/todo2/list")
+@todo2.route("/list")
 def list():
     count = len(todos)
     return render_template("todo2/list.html", todos = todos, count = count)
@@ -12,4 +12,3 @@ def list():
 
 
 
-todo2.run(debug=True)
